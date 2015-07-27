a49cf77f645114fae66c08bf29e674e9737aa35a
Fadi Alnabolsi <fadi@service.com>
2015-07-27 02:08:54 -0400

    finished 3.7. starting 3.8

--- jquery/lesson-14/slider.js
+++ jquery/lesson-14/slider.js
@@ -8,6 +8,37 @@
 		totalImgsWidth = imgWidth * imgsLen; //2400
 
 	$('#slider-nav').show().find('button').on('click', function(){
-		
+		var direction = $(this).data('dir'),
+			loc = imgWidth;
+
+		//update current value
+		( direction === 'next') ? current++ : current--
+
+		//if first iamge
+		if (current === 0) {
+			current = imgsLen;
+			loc = totalImgsWidth - imgWidth;
+			direction = 'next';
+		} else if (current > imgsLen) {
+			//are we at end? should we reset?
+			current = 1;
+			loc = 0;
+		}
+
+		transition(sliderUL, loc, direction);
+
+		console.log(current);
 	});
+
+	function transition( container, loc, direction ) {
+		var unit; //-= or +=
+		if ( direction && loc !== 0 ) {
+			unit = (direction === 'next') ? '-=' : '+='
+		}
+
+		container.animate({
+			'margin-left': unit ? (unit + loc) : loc
+		})
+	}
+
 })(jQuery);
